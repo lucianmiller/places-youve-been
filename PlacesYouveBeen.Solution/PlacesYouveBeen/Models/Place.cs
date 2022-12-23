fa9c354eae157990881c07d19cf2a3e5dcb11150
Lucian Miller <lucian.miller185@gmail.com>
2022-12-23 10:01:08 -0800

    Add place object

---
+++ PlacesYouveBeen.Solution/PlacesYouveBeen/Models/Place.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+
+namespace PlacesYouveBeen.Models
+{
+  public class Place
+  {
+    public string Description { get; set; }
+    public string Id { get; }
+    private static List<Place> _instances = new List<Place>();
+
+    public Place(string description)
+    {
+      Description = description;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static List<Place> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static Place Find(int searchId)
+    {
+      return _instances[searchId - 1];
+    }
+  }
+}
